c20eb505d297a06dcad269a6826997dfb58e13d8
Matvey <m5221211m@gmail.com>
2026-04-20 10:41:06 +0300

    fix: add cors policy in security config

--- src/main/java/com/riloidx/todolist/config/SecurityConfig.java
+++ src/main/java/com/riloidx/todolist/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.
                         sessionCreationPolicy(SessionCreationPolicy.STATELESS))
